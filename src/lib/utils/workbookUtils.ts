237e5a097dfd71443c8baa3c12392c2495b6d584
Infroz <infroz98@gmail.com>
2024-07-21 19:04:14 +0200

    prefer arrow func

--- src/lib/utils/workbookUtils.ts
+++ src/lib/utils/workbookUtils.ts
@@ -6,18 +6,13 @@ export const getSheets = (sheet: Worksheet) => `<sheet name="${sheet.getName()}"
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-const renderRow = (row: Row, index: number) => {
-
-    return `<row r="${index + 1}">
+const renderRow = (row: Row, index: number) => `<row r="${index + 1}">
         ${Object.entries(row).map(([_, value], i) => `<c r="${alphabet[i]}${index + 1}" t="${getCellType(value)}"><v>${value}</v></c>`).join("\n")}
-    </row>`
-}
+    </row>`;
 
-export const getSheetFile = (sheet: Worksheet) => {
-    return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+export const getSheetFile = (sheet: Worksheet) => `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
   <worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
       <sheetData>
          ${sheet.getData().rows.map(renderRow).join("\n")} 
       </sheetData>
-  </worksheet>`
-}
+  </worksheet>`;
