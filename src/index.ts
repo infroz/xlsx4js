54fc57d032e8d4617b2e4da0d924ff98dd0daf33
Infroz <infroz98@gmail.com>
2024-07-26 14:44:32 +0200

    classless version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export { Workbook, Worksheet } from './lib';
+import createWorkbook from "./lib/createWorkbook.js";
+import createWorksheet from "./lib/createWorksheet.js";
+
+export * from "./lib/createWorkbook.js";
+export * from "./lib/createWorksheet.js";
+
+const workbook = createWorkbook({ name: "My Workbook" });
+
+const sheeet = createWorksheet({ name: "My Sheet", rows: [{ test: 123, string: "My Value1"}, { test: 456, string: "My Second Value1"}] });
+
+workbook.addSheet(sheeet);
+
+workbook.write({ file: { filename: "MyWorkbook.xlsx" } });
+
