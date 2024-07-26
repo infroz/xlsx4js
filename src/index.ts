065974457db6c9e9c14ebec64f0a4989a88dd777
Infroz <infroz98@gmail.com>
2024-07-26 15:15:33 +0200

    removed unused

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
 export { createWorkbook, createWorksheet } from "./lib/index.js";
-import { createWorkbook, createWorksheet } from "./lib/index.js";
-
-const workbook = createWorkbook({ name: "My Workbook" });
-
-const sheeet = createWorksheet({ name: "My Sheet", rows: [{ test: 123, string: "My Value1"}, { test: 456, string: "My Second Value1"}] });
-
-workbook.addSheet(sheeet);
-
-workbook.write({ file: { filename: "MyWorkbook.xlsx" } });
 
