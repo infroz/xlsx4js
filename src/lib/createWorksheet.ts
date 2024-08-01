89b7a0450f0549c4efc9cfc69d59e50fb36afef4
Vebjørn Leiros <infroz98@gmail.com>
2024-08-01 18:09:26 +0200

    Dev (#20)
    * prefer arrow func
    
    * new layout without classes
    
    * classless version
    
    * re-added types
    
    * added new files for new structure
    
    * fix
    
    * updated readme
    
    * 0.1.1-0
    
    * removed unused
    
    * 0.1.1-1
    
    * init prettier
    
    * removed unused deps
    
    * using options filename
    
    * update readme.md
    
    * prettier
    
    * 0.1.1-2
    
    * Update index.ts
    
    * Update createWorksheet.ts
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json

--- src/lib/createWorksheet.ts
+++ src/lib/createWorksheet.ts
@@ -1,44 +1,64 @@
+import { Row, isValidType } from "./types/index.js";
 
-import { Row } from "./types/index.js";
+type BaseOptions = { name: string };
 
-type Options = {
-    name: string;
-    rows: Row[];
-}
+type Options = BaseOptions &
+  ({ rows: Row[]; data: never } | { data: object[] | Row[]; rows: never });
 
 export type Worksheet = {
-    rows: Row[];
-    addRow: (row: Row) => void;
-    addRows: (rows: Row[]) => void;
-    getName: () => string;
-    setName: (name: string) => void;
-    getData: () => Row[];
-    getId: () => string;
-    setId: (id: string) => void;
-}
+  rows: Row[];
+  addRow: (row: Row) => void;
+  addRows: (rows: Row[]) => void;
+  getName: () => string;
+  setName: (name: string) => void;
+  getData: () => Row[];
+  getId: () => string;
+  setId: (id: string) => void;
+};
 
-export const createWorksheet = (options?: Options): Worksheet => {
-    // Worksheet state
-    let name = options?.name ?? "Sheet";
-    const rows = options?.rows ?? [];
-    let id: string;
+const genericObjectsToRows = (
+  arr?: { [key: string]: any }[],
+): Row[] | undefined => {
+  return arr?.map((element) => {
+    const newElement: Row = {};
 
-    return {
-        rows: rows,
-        addRow: (row) => {
-            rows.push(row);
-        },
-        addRows: (rows) => {
-            rows.push(...rows);
-        },
-        getName: () => name,
-        setName: (newName) => { name = newName; },
-        getData: () => rows,
-        getId: () => id,
-        setId: (newId) => {
-            id = newId;
-        }
-    };
+    for (let key in element)
+      if (element.hasOwnProperty(key))
+        newElement[key] = isValidType(element[key])
+          ? element[key]
+          : JSON.stringify(element[key]);
+    return newElement;
+  });
+};
+
+export const createWorksheet = <TData extends object[] | Row[]>(
+  data: TData,
+  options?: Options,
+): Worksheet => {
+  // Worksheet state
+  let name = options?.name ?? "Sheet";
+
+  const rows: Row[] = genericObjectsToRows(data) ?? [];
+  let id: string;
+
+  return {
+    rows: rows,
+    addRow: (row) => {
+      rows.push(row);
+    },
+    addRows: (rows) => {
+      rows.push(...rows);
+    },
+    getName: () => name,
+    setName: (newName) => {
+      name = newName;
+    },
+    getData: () => rows,
+    getId: () => id,
+    setId: (newId) => {
+      id = newId;
+    },
+  };
 };
 
 export default createWorksheet;
