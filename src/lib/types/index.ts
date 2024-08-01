2234ec52e2d700b9222a21eef7f02310a4b613c4
Vebjørn Leiros <infroz98@gmail.com>
2024-08-01 18:03:39 +0200

    Update index.ts

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,7 +1,14 @@
 /**
  * Valid data types for a cell
  */
-export type ValidDataType = string | number | boolean | Date | Error | null | undefined;
+export type ValidDataType =
+  | string
+  | number
+  | boolean
+  | Date
+  | Error
+  | null
+  | undefined;
 
 /**
  * Cell type Boolean OpenXML-3.0.1
@@ -55,24 +62,43 @@ export type CellTypeEmpty = "z";
  * Cell type OpenXML-3.0.1
  * https://learn.microsoft.com/en-us/dotnet/api/documentformat.openxml.spreadsheet.cellvalues?view=openxml-3.0.1#fields
  */
-export type CellType = CellTypeBoolean | CellTypeDate | CellTypeError | CellTypeNumber | CellTypeSharedString | CellTypeString | CellTypeInlineString | CellTypeEmpty;
+export type CellType =
+  | CellTypeBoolean
+  | CellTypeDate
+  | CellTypeError
+  | CellTypeNumber
+  | CellTypeSharedString
+  | CellTypeString
+  | CellTypeInlineString
+  | CellTypeEmpty;
+
+export const isValidType = (value: any): boolean => {
+  if (typeof value === "string") return true;
+  if (typeof value === "number") return true;
+  if (typeof value === "boolean") return true;
+  if (value instanceof Date) return true;
+  if (value instanceof Error) return true;
+  if (value === null || value === undefined) return true;
+
+  return false;
+};
 
 /**
- * Get the cell type 
+ * Get the cell type
  * @param value - The value of the cell
  * @returns CellType
  */
 export const getCellType = (value: ValidDataType): CellType => {
-    if (typeof value === "string") return "str";
-    if (typeof value === "number") return "n";
-    if (typeof value === "boolean") return "b";
-    if (value instanceof Date) return "d";
-    if (value instanceof Error) return "e";
-    if (value === null || value === undefined) return "z";
-    throw new Error(`Invalid cell type: ${value}`);
-}
+  if (typeof value === "string") return "str";
+  if (typeof value === "number") return "n";
+  if (typeof value === "boolean") return "b";
+  if (value instanceof Date) return "d";
+  if (value instanceof Error) return "e";
+  if (value === null || value === undefined) return "z";
+  throw new Error(`Invalid cell type: ${value}`);
+};
 
 /**
  * Keep track of the rows in the worksheet
  */
-export type Row = { [key: string]: ValidDataType }; 
+export type Row = { [key: string]: ValidDataType };
